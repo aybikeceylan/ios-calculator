6945adb44ef38134ab7e849fbdc7f4839ac98967
aybike <aybikesayar@gmail.com>
2022-09-29 15:36:08 +0300

    cmmt

--- app.js
+++ app.js
@@ -2,26 +2,25 @@ const btn = document.querySelector(".box")
 const previous = document.querySelector(".previous-display")
 const current = document.querySelector(".current-display")
 const btnArea = document.querySelector(".buttons")
-
-calc = JSON.parse(localStorage.getItem("calc")) || []
+let counter = 0;
 
 btnArea.addEventListener("click", (e) => {
-    if (e.target.classList.contains("box-num")) {
 
+    console.log(e.target.innerHTML);
+    if (e.target.classList.contains("box-num") && counter < 6) {
+        console.log(true)
+        current.innerHTML += e.target.innerHTML
+        counter++
     }
+    if (e.target.classList.contains("box-calc")) {
+        current.innerHTML += e.target.innerHTML
+        console.log(current)
+        previous.innerHTML = current.innerHTML
+        current.innerHTML = ""
+        if (e.target.classList.contains)
+    }
+
 
 })
 
 
-
-// console.log(e.target.innerHTML);
-// if (e.target.classList.contains("box-num")) {
-//     console.log(true)
-//     current.innerHTML = e.target.innerHTML
-//     isFilled = "true"
-//     btnArea.addEventListener("click", (k) => {
-//         if (isFilled && k.target.classList.contains("box-num")) {
-//             current.innerHTML += k.target.innerHTML
-//         }
-//     })
-// }
